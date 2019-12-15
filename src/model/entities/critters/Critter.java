851dacb7669e9b0830754b82136fa2d520d6c4ee
jamesgov <blindmice1o3@gmail.com>
2019-12-14 19:42:16 -0800

    ended up making a few of the constants from the provided equation (used for calculateDamage(int, int) method in Critter class) into floats. Other equations used Math class's sqrt() method, which returns a double. At the very end of the calculations, cast the value from floating point to int. NEXT: Critter-on-Critter damage doing.

--- src/model/entities/critters/Critter.java
+++ src/model/entities/critters/Critter.java
@@ -45,22 +45,14 @@ public class Critter {
         //TODO: attackEffective is used if the move's Type is PHYSICAL
         // if it's SPECIAL, use specialEffective.
         int playerAttackEffective = statsModule.getStatsEffectiveMap().get(StatsModule.Type.ATTACK);
-        int damage = ((((((2 * level) / 5) + 2) * power  * (playerAttackEffective/opponentDefenseEffective)) / 50) + 2);
+        int damage = (int)((((((2f * level) / 5) + 2) * power * ((float)playerAttackEffective/opponentDefenseEffective)) / 50) + 2);
 
+        /*
         System.out.println("damage: " + damage);
-        double damageAsDouble = ((((((2.0 * level) / 5.0) + 2.0) * power  * (((double)playerAttackEffective)/opponentDefenseEffective)) / 50.0) + 2.0);
-        System.out.println("damageAsDouble: " + damageAsDouble);
-        System.out.println("EQUATION && CALCULATIONS FOR calculateDamage(power, opponentDefenseEffective)......");
-        System.out.println("((((((2 * level) / 5) + 2) * power  * (playerAttackEffective/opponentDefenseEffective)) / 50) + 2) ??????");
-        System.out.println("((2 * level) / 5): " + ((2 * level) / 5));
-        System.out.println("(((2 * level) / 5) + 2): " + (((2 * level) / 5) + 2));
-        System.out.println("((((2 * level) / 5) + 2) * power: " + ((((2 * level) / 5) + 2) * power));
-        System.out.println("(playerAttackEffective/opponentDefenseEffective): " + (playerAttackEffective/opponentDefenseEffective));
-        System.out.println("((((2 * level) / 5) + 2) * power  * (playerAttackEffective/opponentDefenseEffective)): " + ((((2 * level) / 5) + 2) * power  * (playerAttackEffective/opponentDefenseEffective)));
-        System.out.println("(((((2 * level) / 5) + 2) * power  * (playerAttackEffective/opponentDefenseEffective)) / 50): " + (((((2 * level) / 5) + 2) * power  * (playerAttackEffective/opponentDefenseEffective)) / 50));
-        System.out.println("****************************************************************************************************");
-        System.out.println("((((((2 * level) / 5) + 2) * power  * (playerAttackEffective/opponentDefenseEffective)) / 50) + 2): " + ((((((2 * level) / 5) + 2) * power  * (playerAttackEffective/opponentDefenseEffective)) / 50) + 2));
-        System.out.println("****************************************************************************************************");
+        float damageAsFloat = ((((((2f * level) / 5) + 2) * power * (((float)playerAttackEffective)/opponentDefenseEffective)) / 50) + 2);
+        System.out.println("damageAsFloat: " + damageAsFloat);
+        System.out.println("(((double)playerAttackEffective)/opponentDefenseEffective): " + (((double)playerAttackEffective)/opponentDefenseEffective));
+        */
 
         //TODO: everything before + (* Modifier)
         //Modifier == Targets * Weather * Badge * Critical * random * STAB * Type * Burn * other.
