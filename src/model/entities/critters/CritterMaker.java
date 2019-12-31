b5692cabeccc2c47f01503fdcbeeb1619f9f79b5
jamesgov <blindmice1o3@gmail.com>
2019-12-31 12:50:33 -0800

    CritterMaker.generateCritter(int, int) has ROUTE02 implemented. Discovered CeruleanCity uses the same tile sprite targets as PalletTown (but PewterCity and other area/places in WorldMap probably have some of their tile search target sprite's RGB values tweaked slightly from what PalletTown uses for RGB values [even if the tile sprite images are, in all outward appearance, the same]).

--- src/model/entities/critters/CritterMaker.java
+++ src/model/entities/critters/CritterMaker.java
@@ -17,42 +17,54 @@ public class CritterMaker {
         //ROUTE01
         if ( (WorldMap.Route.ROUTE01.getX0() < x) && (x <= (WorldMap.Route.ROUTE01.getX1() + Tile.WIDTH)) &&
                 (WorldMap.Route.ROUTE01.getY0() < y) && (y <= (WorldMap.Route.ROUTE01.getY1() + Tile.HEIGHT)) ) {
-            int randomNumberBetweenZeroAndNine = (int)(Math.random() * 10);
-            System.out.println("CritterMaker.generateCritter(int, int)'s randomNumberBetweenZeroAndNine: " + randomNumberBetweenZeroAndNine);
+            int randomNumberBetweenZeroAndNintyNine = (int)(Math.random() * 100);
+            System.out.println("CritterMaker.generateCritter(int, int)'s randomNumberBetweenZeroAndNintyNine: " + randomNumberBetweenZeroAndNintyNine);
 
-            if (randomNumberBetweenZeroAndNine < 3) {
+            if (randomNumberBetweenZeroAndNintyNine < 30) {
                 species = Critter.Species.COLONIAL_MOUSE;
                 level = (int)((Math.random() * 3) + 2); //2-4
-            } else if (randomNumberBetweenZeroAndNine >= 3) {
+            } else if ((30 <= randomNumberBetweenZeroAndNintyNine) && (randomNumberBetweenZeroAndNintyNine < 100)) {
                 species = Critter.Species.MOTLEY_PIGEON;
                 level = (int)((Math.random() * 6) + 2); //2-7
             } else {
                 species = Critter.Species.COASTAL_GULL;
                 level = 6;
             }
-
-            Critter zygote = new Critter(null, species, level);
-
-            return zygote;
         }
         //TODO: the rest of the world.
         //ROUTE02
         else if ( (WorldMap.Route.ROUTE02.getX0() < x) && (x <= (WorldMap.Route.ROUTE02.getX1() + Tile.WIDTH)) &&
                 (WorldMap.Route.ROUTE02.getY0() < y) && (y <= (WorldMap.Route.ROUTE02.getY1() + Tile.HEIGHT)) ) {
+            int randomNumberBetweenZeroAndNintyNine = (int)(Math.random() * 100);
 
-            species = Critter.Species.COASTAL_GULL;
-            level = 6;
-
-            Critter zygote = new Critter(null, species, level);
-
-            return zygote;
+            if (randomNumberBetweenZeroAndNintyNine < 40) {
+                species = Critter.Species.COLONIAL_MOUSE;
+                level = (int)((Math.random() * 2) + 3); //3-4
+            } else if ((40 <= randomNumberBetweenZeroAndNintyNine) && (randomNumberBetweenZeroAndNintyNine < 70)) {
+                species = Critter.Species.MOTLEY_PIGEON;
+                level = (int)((Math.random() * 5) + 3); //3-7 //TODO: should be 3, 5, 7.
+            } else if ((70 <= randomNumberBetweenZeroAndNintyNine) && (randomNumberBetweenZeroAndNintyNine < 85)) {
+                species = Critter.Species.FEMALE_LAPINE;
+                level = (int)((Math.random() * 3) + 4); //4-6 //TODO: should be 4, 6.
+            } else if ((85 <= randomNumberBetweenZeroAndNintyNine) && (randomNumberBetweenZeroAndNintyNine < 100)) {
+                species = Critter.Species.MALE_LAPINE;
+                level = (int)((Math.random() * 3) + 4); //4-6 //TODO: should be 4, 6.
+            } else {
+                species = Critter.Species.COASTAL_GULL;
+                level = 6;
+            }
         }
         //DEFAULT: Species.COASTAL_GULL, 6
         else {
             System.out.println("CritterMaker.generateCritter(int, int)'s else block!");
 
-            return new Critter(null, Critter.Species.COASTAL_GULL, 6);
+            species = Critter.Species.COASTAL_GULL;
+            level = 6;
         }
+
+        Critter zygote = new Critter(null, species, level);
+
+        return zygote;
     }
     //!!!!WILL NEED TO SET HANDLER!!!!
 
